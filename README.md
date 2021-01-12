c9a585fb7034b97350340ce86c48bfbdaaef1bc7
Suryanand <55799779+Suryanandx@users.noreply.github.com>
2021-01-12 11:06:16 +0530

    Initial Build

---
+++ README.md
@@ -0,0 +1,17 @@
+# E-Commerce
+### [Live Site](https://ecomm-website-cms-beta.netlify.app/)
+
+![eCommerce](https://i.ibb.co/mH9SNNq/Build-an-e-commerce-1.png)
+
+## Introduction
+The following app is not having a proper backend ans is powered using a CMS 
+
+## Commands
+
+npm install - To install all dependencies
+
+npm start - To start the project on localhost:3000/
+
+npm run build - To build the production ready product
+
+Have fun!!
---
+++ build/precache-manifest.a00a51cce152d7593dc9f0bd92d2fbe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443fa0055d56aefe886a28379d04fca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e43cd201d09309887ebc",
+    "url": "/static/js/2.d3d99ab2.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.d3d99ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd8c202634f4d173c25",
+    "url": "/static/js/main.ff06a693.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/static/media/commerce.422fec10.png"
+  }
+]);
